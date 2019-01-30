184ec2414258ad1c921af9db646dea268b02d228
oleksiy01234 <oleksiy01234@users.noreply.github.com>
2019-01-30 15:29:37 +1100

    Highest Product of 3.

---
+++ Problems/src/Util.java
@@ -0,0 +1,12 @@
+/**
+ * Util.java contains common utility functions used by many other classes.
+ */
+class Util {
+  static int max(int n1, int n2, int n3) {
+    return Math.max(n1, Math.max(n2, n2));
+  }
+
+  static int min(int n1, int n2, int n3) {
+    return Math.min(n1, Math.min(n2, n2));
+  }
+}
