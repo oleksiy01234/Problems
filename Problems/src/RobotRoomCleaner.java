e2c7d7f6f98169d0b3b55500116f63780a8113b7
oleksiy01234 <oleksiy01234@users.noreply.github.com>
2019-02-11 20:28:02 +1100

    Added several more solutions.

---
+++ Problems/src/RobotRoomCleaner.java
@@ -0,0 +1,70 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class RobotRoomCleaner {
+
+  interface Robot {
+    public boolean move();
+
+    public void turnLeft();
+
+    public void turnRight();
+
+    public void clean();
+  }
+
+  enum Direction {
+    UP, RIGHT, DOWN, LEFT;
+  }
+
+  static void cleanRoom(Robot robot) {
+    Set<String> visited = new HashSet<>();
+    dfs(robot, visited, 0, 0, Direction.UP);
+  }
+
+  private static void dfs(Robot robot, Set<String> visited, int row, int col, Direction curDirection) {
+    String path = row + "," + col;
+    if (visited.contains(path)) {
+      return;
+    }
+
+    visited.add(path);
+    robot.clean();
+
+    for (int i = 0; i < 4; i++) {
+      if (robot.move()) {
+        // go all the way till cannot move, then back one step
+        if (curDirection == Direction.UP) {
+          dfs(robot, visited, row + 1, col, curDirection);
+        } else if (curDirection == Direction.RIGHT) {
+          dfs(robot, visited, row, col + 1, curDirection);
+        } else if (curDirection == Direction.DOWN) {
+          dfs(robot, visited, row - 1, col, curDirection);
+        } else {
+          dfs(robot, visited, row, col - 1, curDirection);
+        }
+
+        robot.turnLeft();
+        robot.turnLeft();
+        robot.move();
+        robot.turnLeft();
+        robot.turnLeft();
+      }
+
+      robot.turnRight();
+      curDirection = turnRight(curDirection);
+    }
+  }
+
+  private static Direction turnRight(Direction curDirection) {
+    if (curDirection == Direction.UP) {
+      return Direction.RIGHT;
+    } else if (curDirection == Direction.RIGHT) {
+      return Direction.DOWN;
+    } else if (curDirection == Direction.DOWN) {
+      return Direction.LEFT;
+    } else {
+      return Direction.UP;
+    }
+  }
+}
---
+++ Problems/src/RomanNumerals.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.AbstractMap.SimpleImmutableEntry;
+
+class RomanNumerals {
+  public static void main(String[] args) {
+    System.out.println(romanToInt("XIX"));
+    System.out.println(intToRoman(1992));
+  }
+
+  private static int romanToInt(String s) {
+    Map<Character, Integer> romanNumerals = generateMap();
+    int result = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      int thisValue = romanNumerals.get(s.charAt(i));
+      if (i < s.length() - 1 && thisValue < romanNumerals.get(s.charAt(i + 1))) {
+        result -= thisValue;
+      } else {
+        result += thisValue;
+      }
+    }
+
+    return result;
+  }
+
+  private static Map<Character, Integer> generateMap() {
+    Map<Character, Integer> romanNumerals = new HashMap<>();
+
+    romanNumerals.put('I', 1);
+    romanNumerals.put('V', 5);
+    romanNumerals.put('X', 10);
+    romanNumerals.put('L', 50);
+    romanNumerals.put('C', 100);
+    romanNumerals.put('D', 500);
+    romanNumerals.put('M', 1000);
+
+    return romanNumerals;
+  }
+
+  private static String intToRoman(int n) {
+    List<SimpleImmutableEntry<Integer, String>> map = generateIntMap();
+    StringBuilder sb = new StringBuilder();
+
+    for (SimpleImmutableEntry<Integer, String> entry : map) {
+      while (n >= entry.getKey()) {
+        sb.append(entry.getValue());
+        n -= entry.getKey();
+      }
+    }
+
+    return sb.toString();
+  }
+
+  private static List<SimpleImmutableEntry<Integer, String>> generateIntMap() {
+    List<SimpleImmutableEntry<Integer, String>> list = new ArrayList<>();
+
+    list.add(new SimpleImmutableEntry<>(1000, "M"));
+    list.add(new SimpleImmutableEntry<>(900, "CM"));
+    list.add(new SimpleImmutableEntry<>(500, "D"));
+    list.add(new SimpleImmutableEntry<>(400, "CD"));
+    list.add(new SimpleImmutableEntry<>(100, "C"));
+    list.add(new SimpleImmutableEntry<>(90, "XC"));
+    list.add(new SimpleImmutableEntry<>(50, "L"));
+    list.add(new SimpleImmutableEntry<>(40, "XL"));
+    list.add(new SimpleImmutableEntry<>(10, "X"));
+    list.add(new SimpleImmutableEntry<>(9, "IX"));
+    list.add(new SimpleImmutableEntry<>(5, "V"));
+    list.add(new SimpleImmutableEntry<>(4, "IV"));
+    list.add(new SimpleImmutableEntry<>(1, "I"));
+
+    return list;
+  }
+
+}
